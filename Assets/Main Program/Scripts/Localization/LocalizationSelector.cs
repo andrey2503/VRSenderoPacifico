78862b5c2526bde3042acec9de4d7bea1f96f5a3
Michael <mvrd_17@hotmail.com>
2019-04-10 23:05:30 -0600

    Translation system

---
+++ Assets/Main Program/Scripts/Localization/LocalizationSelector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadLocalizationFile(string text)
+    {
+        LocalizationManager.instance.LoadLocalizedText(text);
+    }
+}
