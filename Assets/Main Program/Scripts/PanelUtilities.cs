b23b5482ba1166aeae778d07a54e9e8bb6003af9
Michael <mvrd_17@hotmail.com>
2019-05-23 22:36:45 -0600

    UI improvements

--- Assets/Main Program/Scripts/PanelUtilities.cs
+++ Assets/Main Program/Scripts/PanelUtilities.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class PanelUtilities : MonoBehaviour
 {
-    
-    public GameObject panel0;
-    public GameObject panel1;
-    public GameObject panel2;
-    public GameObject panel3;
-    public GameObject panel4;
-    public GameObject panel5;
-    public GameObject panel6;
-
 
     public void hidePanel(GameObject panel) {
         panel.SetActive(false);
--- Assets/Main Program/Scripts/TouchControl.cs
+++ Assets/Main Program/Scripts/TouchControl.cs
@@ -1,79 +1,38 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class TouchControl : MonoBehaviour
-{
-    public float movementSpeed = 800f;
-    private Vector3 position;
-    private float width;
-    private float height;
-
-    private float xHelper = 0f;
-    private float yHelper = 0f;
-
-    void Awake()
-    {
-        width = (float)Screen.width / 2.0f;
-        height = (float)Screen.height / 2.0f;
-
-        // Position used for the cube.
-        position = new Vector3(0.0f, 0.0f, 0.0f);
-    }
-
-    void OnGUI()
-    {
-        // Compute a fontSize based on the size of the screen width.
-        GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
-
-        GUI.Label(new Rect(20, 20, width, height * 0.25f),
-            "x = " + position.x.ToString("f2") +
-            ", y = " + position.y.ToString("f2"));
-    }
-
-    void Update()
-    {
-        // Handle screen touches.
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            // Move the cube if the screen has the finger moving.
-            if (touch.phase == TouchPhase.Moved)
-            {
-                Vector2 pos = touch.position;
-                pos.x = (pos.x - width) / width;
-                pos.y = (pos.y - height) / height;
-                position = new Vector3(pos.x, pos.y, 0f);
-
-
-                // Position the cube.
-
-                transform.localPosition = position * movementSpeed;
-                // transform.localPosition = new Vector3(
-                //     (xHelper < pos.x ? transform.localPosition.x + movementSpeed : transform.localPosition.x - movementSpeed),
-                //     (yHelper < pos.y ? transform.localPosition.y + movementSpeed : transform.localPosition.y - movementSpeed),
-                //     0f);
-
-                xHelper = pos.x;
-                yHelper = pos.y;
-            }
-
-            // if (Input.touchCount == 2)
-            // {
-            //     touch = Input.GetTouch(1);
-
-            //     if (touch.phase == TouchPhase.Began)
-            //     {
-            //         // Halve the size of the cube.
-            //         transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-            //     }
-
-            //     if (touch.phase == TouchPhase.Ended)
-            //     {
-            //         // Restore the regular size of the cube.
-            //         transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-            //     }
-            // }
+public class TouchControl : MonoBehaviour {
+    Vector3 touchStart;
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 8;
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+        if(Input.touchCount == 2){
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 0.01f);
+        }else if(Input.GetMouseButton(0)){
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+        zoom(Input.GetAxis("Mouse ScrollWheel"));
+	}
+
+    void zoom(float increment){
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
     }
 }
